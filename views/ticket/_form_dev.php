034f37d6c25d0055f5f854d0132719cb6967550e
chaoos <chaoos@users.noreply.github.com>
2017-06-13 16:49:44 +0200

    dev items added

---
+++ views/ticket/_form_dev.php
@@ -0,0 +1,21 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+use kartik\datetime\DateTimePicker;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Ticket */
+/* @var $form yii\widgets\ActiveForm */
+/* @var $searchModel app\models\TicketSearch */
+
+?>
+<hr>
+<div class="row dev_item">
+    <div class="col-md-6">
+        <?= $form->field($model, 'download_lock')->checkBox(); ?>
+        <?= $form->field($model, 'backup_lock')->checkbox() ?>
+        <?= $form->field($model, 'restore_lock')->checkbox() ?>
+        <?= $form->field($model, 'bootup_lock')->checkbox() ?>
+    </div>
+</div>
