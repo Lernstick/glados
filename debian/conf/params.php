0dfa104f0ec4faad4c17b6b723eb0c725f28baee
chaoos <chaoos@users.noreply.github.com>
2019-03-20 18:10:40 +0100

    version 1.0.5 2nd commit

--- debian/conf/params.php
+++ debian/conf/params.php
@@ -1,12 +1,13 @@
 <?php
 return [
-    'version' =>                '1.0.4',
+    'version' =>                '1.0.5',
     'itemsPerPage' =>           10,
     'tokenLength' =>            10,
     'uploadPath' =>             '/var/lib/glados/uploads/',
     'backupPath' =>             '/var/lib/glados/backups/',
     'resultPath' =>             '/var/lib/glados/results/',
     'tmpPath' =>                '/var/lib/glados/tmp/',
+    'sciptsPath' =>             '/usr/share/glados/scripts/',
     'dotSSH' =>                 '/var/lib/glados/.ssh/',
     'daemonLogFilePath' =>      '/var/log/glados',
     'examDownloadBandwith' =>   '10m', // 10MB per second, set 0 for no limit
