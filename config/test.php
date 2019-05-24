b01e94a63b90b9e1d25efaaa8ba12a34475c932e
chaoos <p1020389@yahoo.com>
2019-05-24 09:15:05 +0200

    perform tests in test db

--- config/test.php
+++ config/test.php
@@ -1,17 +1,17 @@
 <?php
-$params = require __DIR__ . '/params.php';
+/**
+ * Application configuration shared by all test types
+ */
 $db = require __DIR__ . '/test_db.php';
 $config = require(__DIR__ . '/web.php');
 
 $config['id'] = 'basic-tests';
 $config['components']['request']['cookieValidationKey'] = 'test';
 $config['components']['request']['enableCsrfValidation'] = false;
+$config['components']['db'] = $db;
 // but if you absolutely need it set cookie domain to localhost
 /*$config['components']['request']['csrfCookie'] = [
     'domain' => 'localhost',
 ];*/
 
-/**
- * Application configuration shared by all test types
- */
 return $config;
