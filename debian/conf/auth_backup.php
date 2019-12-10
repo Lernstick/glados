5f9a68798ecde0963dab86753b8fa7d7530af180
chaoos <p1020389@yahoo.com>
2019-12-10 16:44:09 +0100

    debian/conf/auth_backup.php added

---
+++ debian/conf/auth_backup.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Please to not edit this file.
+ * This file was automatically generated using the web interface.
+ */
+return [
+  'class' => 'app\models\Auth',
+  'methods' => 
+    array (
+    )
+];
