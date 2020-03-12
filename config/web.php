517228e16a07c6782ee96f125a14efd75d6facaa
chaoos <roman.gruber@inf.unibe.ch>
2020-03-12 16:59:37 +0100

    version 1.0.7

--- config/web.php
+++ config/web.php
@@ -5,7 +5,7 @@ $params = require(__DIR__ . '/params.php');
 $config = [
     'id' => 'basic',
     'name' => 'GLaDOS',
-    'version' => '1.0.6',
+    'version' => '1.0.7',
     'basePath' => dirname(__DIR__),
     'bootstrap' => [
         'log',
