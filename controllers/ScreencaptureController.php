69fdbf17006e9a3f34a330cb3fb4ccb61477658f
chaoos <p1020389@yahoo.com>
2020-06-03 11:30:00 +0200

    removed subtitle tests

--- controllers/ScreencaptureController.php
+++ controllers/ScreencaptureController.php
@@ -59,32 +59,6 @@ class ScreencaptureController extends Controller
         $ticket = Ticket::findOne($id);
         if (($model = $ticket->screencapture) !== null) {
 
-
-            if ($file == "subtitles.webvtt") {
-                return Yii::$app->response->sendContentAsFile("WEBVTT
-
-1
-00:00:01.500 --> 00:00:03.500
-When the moon <00:00:02.500>hits your eye
-
-2
-00:00:03.500 --> 00:00:05.500
-Like a <00:00:04.000>big-a <00:00:04.500>pizza <00:00:05.000>pie
-
-3
-00:00:05.500 --> 00:00:06.500
-That's <00:00:06.000>amore
-
-4
-00:00:07.000 --> 00:00:55.500
-Long living <c.class><v delay-500>text</v></c>
-
-", "subtitles.vtt", [
-                    'mimeType' => "text/vtt",
-                    'inline' => false,
-                ]);
-            }
-
             if ( ($contents = $model->getFile($file)) !== null ) {
                 return Yii::$app->response->sendContentAsFile($model->getFile($file), basename($file), [
                     'mimeType' => $model->getMimeType($file),
