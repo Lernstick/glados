0ccbd40b5d17bce0952a3f68fa7a7180c3664aad
chaoos <p1020389@yahoo.com>
2020-11-12 14:33:35 +0100

    screenshot service added in the client

--- scripts/prepare.d/05-screenshots.sh
+++ scripts/prepare.d/05-screenshots.sh
@@ -6,14 +6,7 @@
 function screenshots()
 {
   # config->screenshots
-  if [ "$(config_value "screenshots")" = "False" ]; then
-    chroot ${initrd}/newroot sed -i 's/BackupScreenshot=.*/BackupScreenshot=false/' /etc/lernstickWelcome 
-  else
-    # write/append config options
-    bf=$(config_value "screenshots_interval")
-    chroot ${initrd}/newroot sed -i '/^BackupScreenshot=/{h;s/=.*/=true/};${x;/^$/{s//BackupScreenshot=true/;H};x}' /etc/lernstickWelcome
-    chroot ${initrd}/newroot sed -i '/^Backup=/{h;s/=.*/=true/};${x;/^$/{s//Backup=true/;H};x}' /etc/lernstickWelcome
-    chroot ${initrd}/newroot sed -i '/^BackupFrequency=/{h;s/=.*/='$bf'/};${x;/^$/{s//BackupFrequency='$bf'/;H};x}' /etc/lernstickWelcome
-    chroot ${initrd}/newroot sed -i '/^BackupSource=/{h;s/=.*/=\/home\/user\//};${x;/^$/{s//BackupSource=\/home\/user\//;H};x}' /etc/lernstickWelcome
+  if [ "$(config_value "screenshots")" = "True" ]; then
+    chroot ${initrd}/newroot systemctl enable screenshot.service
   fi
 }
