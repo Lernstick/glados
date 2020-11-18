4955b1d9f08a430c54af578aa5bb8bf93eea4d80
chaoos <p1020389@yahoo.com>
2020-11-18 17:00:15 +0100

    version 1.0.8 changes

--- debian/conf/params.php
+++ debian/conf/params.php
@@ -1,19 +1,2 @@
 <?php
-return [
-    'itemsPerPage' =>           10,
-    'uploadPath' =>             '/var/lib/glados/uploads/',
-    'backupPath' =>             '/var/lib/glados/backups/',
-    'resultPath' =>             '/var/lib/glados/results/',
-    'tmpPath' =>                '/var/lib/glados/tmp/',
-    'scPath' =>                 '/var/lib/glados/sc/',
-    'sciptsPath' =>             '/usr/share/glados/scripts/',
-    'dotSSH' =>                 '/var/lib/glados/.ssh/',
-    'daemonLogFilePath' =>      '/var/log/glados',
-    'examDownloadBandwith' =>   '10m', // 10MB per second, set 0 for no limit
-    'minDaemons' =>             3,
-    'maxDaemons' =>             10,
-    'upperBound' =>             80,
-    'lowerBound' =>             20,
-    'abandonTicket' =>          10800, // leave the ticket after 3 hours of failed backup attempts
-    'liveEvents' =>             true, // enable or disable live data
-];
+return [];
